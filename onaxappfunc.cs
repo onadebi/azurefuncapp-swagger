afa93454b22ca0d382c9d94a10758fa66b738f52
Onaefe.Edebi <onaefe@onaxsys.com>
2025-07-02 17:00:55 -0400

    Addition of more endpoints

--- onaxappfunc.cs
+++ onaxappfunc.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using Microsoft.OpenApi.Models;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Extensions.Primitives;
 
 
 namespace OnaxFiles;
@@ -20,11 +21,34 @@ public class OnaxAppFunc
 
     [Function("GetIntro")]
     [OpenApiOperation(operationId: "Run", tags: new[] { "hello" })]
-    [OpenApiParameter(name: "GetIntro", In = ParameterLocation.Query, Required = false, Type = typeof(string), Description = "Your name")]
+    [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = false, Type = typeof(string), Description = "Your name")]
     [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The greeting")]
     public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!");
+        req.Query.TryGetValue("name", out StringValues username);
+        return new OkObjectResult("Welcome to Azure Functions " + (username.Count != 0 ? username.ToString() : string.Empty));
+    }
+
+    [Function("SendSMS")]
+    [OpenApiOperation(operationId: "Run", tags: new[] { "hello" })]
+    [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = false, Type = typeof(string), Description = "Your name")]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The greeting")]
+    public IActionResult SendSMS([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        req.Query.TryGetValue("name", out StringValues username);
+        return new OkObjectResult("Welcome to Azure Functions " + (username.Count != 0 ? username.ToString() : string.Empty));
+    }
+
+    [Function("Webhook")]
+    [OpenApiOperation(operationId: "Run", tags: new[] { "hello" })]
+    [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = false, Type = typeof(string), Description = "Your name")]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The greeting")]
+    public IActionResult Webhook([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        req.Query.TryGetValue("name", out StringValues username);
+        return new OkObjectResult("Welcome to Azure Functions " + (username.Count != 0 ? username.ToString() : string.Empty));
     }
 }
